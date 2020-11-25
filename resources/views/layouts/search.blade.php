dc21adcdffcf10cda2c98330f34afa294c653b59
Le Xuan Quang <Quang.lxun@gmail.com>
2020-11-25 16:20:48 +0700

    layouts login register

---
+++ resources/views/layouts/search.blade.php
@@ -0,0 +1,14 @@
+<div class="banner_1">
+    <div class="container">
+        <div id="search_wrapper1">
+            <div id="search_form" class="clearfix">
+                <h1>@lang('search.title')</h1>
+                <p>
+                    <input type="text" class="text" placeholder="@lang('search.keywords')" value="">
+                    <input type="text" class="text" placeholder="@lang('search.company')" value="">
+                    <label class="btn2 btn-2 btn2-1b"><input type="submit" value="@lang('search.find')"></label>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>

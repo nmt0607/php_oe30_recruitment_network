74326ce6a895bb59f438913f7683df0fc19be91d
Le Xuan Quang <Quang.lxun@gmail.com>
2020-12-09 09:01:10 +0700

    edit user and company

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,15 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    public function imageable()
-    {
-        return $this->morphTo();
-    }
-
     protected $fillable = [
         'url',
         'imageable_id',
         'imageable_type',
         'type',
     ];
+
+    public function imageable()
+    {
+        return $this->morphTo();
+    }
 }
